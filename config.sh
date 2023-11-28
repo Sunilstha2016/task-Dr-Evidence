6cd913671cc24744841a76826e88933759a92604
Sunilstha2016 <sunil@expressmail.io>
2023-11-28 14:14:33 -0600

    Updated codes

---
+++ config.sh
@@ -0,0 +1,16 @@
+######Step 5: Configure Vault##############
+
+##########In a real-world scenario, you would use the Vault HTTP API to store and retrieve secrets securely. #################
+
+#########For this example, let's use the Vault command line.###################
+
+export VAULT_ADDR=http://127.0.0.1:8200
+export VAULT_TOKEN=myroot
+
+##### Store a secret  ######
+
+vault kv put secret/myapp/config username=admin password=secretpassword
+
+######  Retrieve a secret  ######
+
+vault kv get secret/myapp/config
